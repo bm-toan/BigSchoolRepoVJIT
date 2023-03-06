c49195bc83717d60e013f2584b3addbb38569bdd
Toan Bui <toanbui@kms-technology.com>
2023-03-06 11:21:31 +0700

    Add Index2 action to HomeController

--- BigSchool/Controllers/HomeController.cs
+++ BigSchool/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace BigSchool.Controllers
             return View();
         }
 
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
