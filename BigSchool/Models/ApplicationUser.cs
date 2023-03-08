e8caf592de3a291937e850c39802b8867f6efc74
Toan Bui <toanbui@kms-technology.com>
2023-03-08 11:15:08 +0700

    add migration named CreateTableCategoryCourse

--- BigSchool/Models/ApplicationUser.cs
+++ BigSchool/Models/ApplicationUser.cs
@@ -18,16 +18,4 @@ namespace BigSchool.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("asdasdasdasd", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
 }
